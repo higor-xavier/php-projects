24f0e5d7a6442d9d89c07ae4ef0ffedf427c3d4c
unknown <higorsxavier@gmail.com>
2022-03-08 20:29:07 -0300

    First commit to my php projects folder

---
+++ comentarios.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+		
+		<?php
+			// Comentário de 1 linha
+			echo 'Comando echo <br>';
+
+			# Comentário de 1 linha
+			print 'Comando print <hr>';
+
+			/* Comentários de múltiplas linhas
+			Esse tipo de comentário permite que várias linhas
+			sejam comentadas */
+			
+		?>
+
+	</body>	
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+
+		<?php
+			echo 'Utilizando a tag padrão';
+		?>
+		<br>
+		<!-- Tag de impressão -->
+		<?= 'Utilizando a tag de impressão' ?>
+		<br>
+		<!-- Tag curta (descontinuada) -->
+		<? 
+			echo 'Utilizando a tag curta'; 
+		?>
+
+	</body>	
+</html>
