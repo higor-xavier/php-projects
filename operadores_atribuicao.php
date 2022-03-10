f8fd23c5ce1905384c71f0ce5d5263df1f942189
unknown <higorsxavier@gmail.com>
2022-03-10 16:07:09 -0300

    Arithmetic operators and their particularities

---
+++ operadores_atribuicao.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+
+		<?php 
+			
+			$x = 10;
+			
+			$x += 5;
+
+			echo $x;
+			echo '<br>';
+
+			$x -= 5;
+
+			echo $x;
+			echo '<br>';
+
+			$x *= 5;
+
+			echo $x;
+			echo '<br>';
+
+			$x /= 5;
+
+			echo $x;
+			echo '<br>';
+
+			$x %= 5;
+
+			echo $x;
+			echo '<br>';
+
+		 ?>	
+
+	</body>	
+</html>
---
+++ operadores_incremento_decremento.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+
+		<?php 
+			
+			$x = 7;
+
+			//pre-incremento
+			++$x;
+			echo "$x <br>";
+
+			//pos-incremento
+			$x++;
+			echo "$x <br>";
+
+			//pre-decremento
+			--$x;
+			echo "$x <br>";
+
+			//pos-decremento
+			$x--;
+			echo "$x <br>";
+
+		 ?>	
+
+	</body>	
+</html>
