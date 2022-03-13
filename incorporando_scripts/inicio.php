740550ae6f02174316e32fdba646ff6c96d2b135
Higor Xavier <higorsxavier@gmail.com>
2022-03-13 15:16:13 -0300

    Refactoring the project using require_once

---
+++ incorporando_scripts/inicio.php
@@ -0,0 +1,11 @@
+<?php 
+	include_once("menu.php"); //WARNING = Se não encontra a página passada
+	//require("menu2.php"); //FATAL ERROR = Se não encontra a página passada(interrompe o processo do script completamente)
+ ?>
+conteúdo da página (início)
+<br>
+
+<?php 
+	include_once("menu.php"); //O once descarta a inclusão de um mesmo script na mesma página
+	//require_once(); //O mesmo ocorre para o require_once
+ ?>	
