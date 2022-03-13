fc78de4a870cff3ff76331401439ccca0305d339
Higor Xavier <higorsxavier@gmail.com>
2022-03-13 12:27:21 -0300

    User authentication

--- app-help-desk/valida_login.php
+++ app-help-desk/valida_login.php
@@ -1,11 +1,39 @@
 <?php 
 	
-	print_r($_POST);
+	//variável que verifica se a autenticação foi realizada
+	$usuario_autenticado = false;
+	
+	$usuarios_app = [
+		['email' => 'adm@teste.com.br', 'senha' => '123456'],
+		['email' => 'user@teste.com.br', 'senha' => 'abcd']
+	];
 
-	echo "<br>";
+	/*echo "<pre>";
+		print_r($usuarios_app);
+	echo "</pre>";*/
 
-	echo $_POST['email'];
-	echo "<br>";
-	echo $_POST['senha'];
+	foreach ($usuarios_app as $user) {
+
+		echo "Usuário app: ". $user['email'] . "/" . $user['senha'];
+		echo "<br>";
+		echo "Usuário form: ".$_POST['email']. "/" .$_POST['senha'];
+		echo "<hr>";
+
+		if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+			$usuario_autenticado = true;
+		}
+		
+
+	}
+
+	if ($usuario_autenticado) {
+		echo 'Usuário autenticado';
+	}
+	else{
+		header('Location: index.php?login=erro');
+	}
+
+	/*print_r($_POST);
+	echo "<br>";*/
 
  ?>
