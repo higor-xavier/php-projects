a437c254ffa80b08d59570982fdc5c2fcf35384f
Higor Xavier <higorsxavier@gmail.com>
2022-03-13 19:36:51 -0300

    Applying profiles and their permissions

--- app-help-desk/consultar_chamado.php
+++ app-help-desk/consultar_chamado.php
@@ -1,26 +1,41 @@
-<?php 
-  
-  require_once("validador_acesso.php");
-    
-?>
+<?php require_once "validador_acesso.php" ?>
 
-<?php 
+<?php
 
   //chamados
-  $chamados = [];
+  $chamados = array();
 
+  //abrir o arquivo.hd
   $arquivo = fopen('arquivo.txt', 'r');
 
-  //enquanto houver registros a serem recuperados, iterar
-  while (!feof($arquivo)) { //testa pelo fim de um arquivo
-    //linhas
+  //enquanto houver registros (linhas) a serem recuperados
+  while(!feof($arquivo)) { //testa pelo fim de um arquivo
+    //linhas  
     $registro = fgets($arquivo);
-    $chamados[] = $registro;
+
+    //explode dos detalhes do registro para verificar o id do usuário responsável pelo cadastro
+    $registro_detalhes = explode('#', $registro);
+
+    //(perfil id = 2) só vamos exibir o chamado, se ele foi criado pelo usuário
+    if($_SESSION['perfil_id'] == 2) {
+
+      //se usuário autenticado não for o usuário de abertura do chamado então não faz nada
+      if($_SESSION['id'] != $registro_detalhes[0]) {
+        continue; //não faz nada
+
+      } else {
+        $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+      }
+
+    } else {
+      $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+    }
+
   }
 
   //fechar o arquivo aberto
   fclose($arquivo);
-
+  
 ?>
 
 <html>
@@ -64,32 +79,28 @@
             
             <div class="card-body">
 
-              <?php 
-                //abrindo aqui e fechando lá embaixo para repetir o card e não mesclar o PHP com HTML
-                foreach ($chamados as $chamado) { 
-
-              ?>
-
-              <?php 
-
-                $chamado_dados = explode('#', $chamado);
-
-                if (count($chamado_dados) < 3) {
-                  continue;
-                }
-
-               ?>
+              <?php foreach($chamados as $chamado) { ?>
               
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
-                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
-                  <p class="card-text"><?= $chamado_dados[2] ?></p>
-                </div>
-              </div>
+                <?php
 
-              <?php } //fechando aqui
-               ?>
+                  $chamado_dados = explode('#', $chamado);
+
+                  //não existe detalhes do chamado se ele não estiver completo
+                  if(count($chamado_dados) < 3) {
+                    continue;
+                  }
+
+                ?>
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?=$chamado_dados[1]?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[2]?></h6>
+                    <p class="card-text"><?=$chamado_dados[3]?></p>
+
+                  </div>
+                </div>
+
+              <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
--- app-help-desk/valida_login.php
+++ app-help-desk/valida_login.php
@@ -4,16 +4,24 @@
 
 	//variável que verifica se a autenticação foi realizada
 	$usuario_autenticado = false;
+	$usuario_id = null;
+	$usuario_perfil_id = null;
+
+	$perfis = [1 => 'Administrativo', 2 => 'Usuário'];
 	
 	$usuarios_app = [
-		['email' => 'adm@teste.com.br', 'senha' => '123456'],
-		['email' => 'user@teste.com.br', 'senha' => 'abcd']
+		['id'=> 1,'email' => 'adm@teste.com.br', 'senha' => '123456', 'perfil_id' => 1],
+		['id'=> 2, 'email' => 'user@teste.com.br', 'senha' => 'abcd', 'perfil_id' => 1],
+		['id'=> 3, 'email' => 'jose@teste.com.br', 'senha' => 'abcd', 'perfil_id' => 2],
+		['id'=> 4, 'email' => 'maria@teste.com.br', 'senha' => 'abcd', 'perfil_id' => 2]
 	];
 
 	foreach ($usuarios_app as $user) {
 
 		if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
 			$usuario_autenticado = true;
+			$usuario_id = $user['id'];
+			$usuario_perfil_id = $user['perfil_id'];
 		}
 		
 	}
@@ -21,8 +29,8 @@
 	if ($usuario_autenticado) {
 		echo 'Usuário autenticado';
 		$_SESSION['autenticado'] = 'SIM';
-		$_SESSION['x'] = 'um valor';
-		$_SESSION['y'] = 'outro valor';
+		$_SESSION['id'] = $usuario_id;
+		$_SESSION['perfil_id'] = $usuario_perfil_id;
 		header('Location: home.php');
 	}
 	else{
